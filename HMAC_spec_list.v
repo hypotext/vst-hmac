ecc6ec7b6c4c2b5c8a47f93bf3c17858de7e18d8
Lennart Beringer <eberinge@princeton.edu>
2015-02-12 14:05:23 -0500

    Filled the hole in line -5 of main theorem of this file, and added several auxiliary lemmas

--- HMAC_spec_list.v
+++ HMAC_spec_list.v
@@ -80,6 +80,11 @@ Proof.
   apply leb_complete_conv in teq0.
   rewrite skipn_length; omega.
 Qed.
+(*Lemma toBlocks_consD h t: toBlocks (cons h t) = 
+      if leb (length (cons h t) ) 511 then [firstn 512 (cons h t) ] 
+      else firstn 512 (cons h t)  :: toBlocks (skipn 512 (cons h t) ).
+Proof. rewrite toBlocks_equation. reflexivity. Qed.*)
+
 (*Function toBlocks (l : Blist) {measure length l} : list Blist :=
   match l with
     | nil => nil
@@ -102,6 +107,7 @@ Proof.
   
 Admitted.
 *)
+
 Definition sha_splitandpad_blocks (msg : Blist) : list Blist :=
   toBlocks (sha_splitandpad_inc msg).
 
@@ -158,6 +164,76 @@ Proof.
 Qed.
 *)
 
+(*NEW*) Require Import pure_lemmas.
+(*NEW*) Require Import Coqlib.
+(*NEW*) Require Import Integers.
+
+(*NEW*) Lemma sha_splitandpad_inc_nil: length (sha_splitandpad_inc nil) = 512%nat.
+Proof. reflexivity. Qed.
+
+(*NEW*) Lemma add_blocksize_length l n: 0<=n ->
+      BinInt.Z.add n (Zcomplements.Zlength l) = Zcomplements.Zlength ((Coqlib.list_repeat (Z.to_nat n) true) ++ l).
+Proof. intros. do 2 rewrite Zlength_correct.
+  rewrite app_length, length_list_repeat, Nat2Z.inj_add, Z2Nat.id; trivial.
+Qed. 
+
+(*NEW*) Lemma pad_inc_length: forall l, exists k, (0 < k /\ length (pad_inc l) = k*64)%nat.
+Proof. unfold pad_inc.
+  induction l. 
+  simpl. exists (1%nat). omega.
+  destruct IHl as [k [K HK]]. repeat rewrite app_length in *. rewrite length_list_repeat in *.
+  rewrite pure_lemmas.length_intlist_to_Zlist in *.
+  remember (BinInt.Z.to_nat
+        (BinInt.Z.modulo
+           (BinInt.Z.opp
+              (BinInt.Z.add (BinInt.Z.add BlockSize (Zcomplements.Zlength l))
+                 9)) 64)).
+  remember (BinInt.Z.to_nat
+      (BinInt.Z.modulo
+         (BinInt.Z.opp
+            (BinInt.Z.add
+               (BinInt.Z.add BlockSize (Zcomplements.Zlength (a :: l))) 9))
+         64)).
+  simpl. simpl in HK.
+  assert ((BinInt.Z.add
+                   (BinInt.Z.add BlockSize (Zcomplements.Zlength (a :: l))) 9) =
+          BinInt.Z.add 1 (BinInt.Z.add
+                   (BinInt.Z.add BlockSize (Zcomplements.Zlength l)) 9)).
+        rewrite BinInt.Z.add_assoc. f_equal.
+          rewrite BinInt.Z.add_assoc. rewrite (BinInt.Z.add_comm 1). rewrite <- (BinInt.Z.add_assoc _ 1).
+          f_equal. 
+          repeat rewrite Zcomplements.Zlength_correct. SearchAbout BinInt.Z.add BinInt.Z.of_nat.
+          apply (Znat.Nat2Z.inj_add 1 (length l)).
+   rewrite H in Heqn0; clear H. 
+   remember (BinInt.Z.add (BinInt.Z.add BlockSize (Zcomplements.Zlength l)) 9). clear Heqz.
+   subst n n0. rewrite Z.opp_add_distr. rewrite <- (Z.add_comm (-z)). remember (-z) as zz. clear Heqzz.
+   simpl.
+   destruct (zeq (zz mod 64) 0).
+     rewrite e in HK.
+     assert ((zz+-1) mod 64 = 63). clear - e.  apply Zmod_divides in e. 2:omega. 
+        destruct e. subst. rewrite Zplus_mod. rewrite Z.mul_comm. rewrite Z_mod_mult. simpl.
+        rewrite Zmod_mod. apply Zmod_unique with (a:=(-1)). omega. omega.
+     rewrite H. clear H e. simpl in *. exists (S k). omega.
+   assert ((zz + -1) mod 64 = (zz mod 64) - 1 /\ 0 <= (zz mod 64) - 1). 
+     clear -n. rewrite Zplus_mod. assert (0 <= zz mod 64 < 64). apply Z.mod_pos_bound. omega.
+     split. 2: omega.
+     symmetry. eapply Z.mod_unique. left. omega.
+     assert (63 = -1 mod 64). eapply Z.mod_unique. left; omega. instantiate (1:=-1). omega.
+     rewrite <- H0. instantiate (1:=1). omega.
+   destruct H. rewrite H. clear H.   
+   assert (Z.to_nat (zz mod 64 - 1) = minus (Z.to_nat (zz mod 64)) 1).
+     clear - n H0. remember (zz mod 64).  clear Heqz. rewrite Z2Nat.inj_sub. reflexivity. omega. 
+   rewrite H; clear H. rewrite <- NPeano.Nat.add_sub_swap. rewrite minus_Sn_m. simpl. exists k. omega.
+   omega. apply (Z2Nat.inj_le 1). omega. omega. omega.
+Qed. 
+
+(*NEW*) Lemma sha_splitandpad_inc_length: forall m, exists k, 
+      (0<k /\ length (sha_splitandpad_inc m) = k * 512)%nat.
+Proof. intros. unfold sha_splitandpad_inc.
+  destruct (pad_inc_length (bitsToBytes m)) as [k [K HK]].
+  rewrite bytesToBits_len, HK. exists k. split. trivial. omega.
+Qed.
+
 (* since sha_splitandpad_inc is used instead of the modified version in the Concat-Pad proof *)
 (* TODO: go through and verify that all the proofs chain *)
 Lemma sha_splitandpad_inc_eq : forall (msg : Blist),
@@ -165,14 +241,95 @@ Lemma sha_splitandpad_inc_eq : forall (msg : Blist),
 Proof.
   intros msg.
   unfold sha_splitandpad_inc'. unfold sha_splitandpad_blocks.
-  symmetry.
-  apply concat_toBlocks_id.
-  * (* InBlocks 512 (sha_splitandpad_inc msg) *)
-    admit.
+  rewrite toBlocks_equation. 
+  remember (sha_splitandpad_inc msg). 
+  destruct b. reflexivity. unfold sha_splitandpad_inc in Heqb. rewrite Heqb. clear Heqb.
+  destruct (sha_splitandpad_inc_length msg) as [k [K HK]].
+  unfold sha_splitandpad_inc in HK. rewrite HK.
+  rewrite leb_correct_conv. 2: omega.
+  remember (pad_inc (bitsToBytes msg)). clear Heql.
+  symmetry. 
+  assert (HH: firstn 512 (bytesToBits l) :: toBlocks (skipn 512 (bytesToBits l)) = toBlocks  (bytesToBits l)).
+    remember (bytesToBits l) as bits. rewrite (toBlocks_equation bits).
+    destruct bits. destruct l; simpl in *. omega. discriminate.
+    (*rewrite Heqbits. clear Heqbits. *)
+    rewrite leb_correct_conv. trivial. rewrite HK. omega. 
+  rewrite HH. apply concat_toBlocks_id.
+  apply InBlocks_len. rewrite HK. exists k. omega.
+Qed.
 
-Qed.  
+(*NEW*) Lemma length_mul_split A k (K:(0<k)%nat) n (N:(0<n)%nat): forall (l:list A), length l = (k * n)%nat -> 
+      exists l1, exists l2, l=l1++l2 /\ length l1=n /\ length l2 = ((k-1) * n)%nat.
+Proof.
+  intros. 
+  assert ((k * n = n + (k-1) * n)%nat). rewrite mult_minus_distr_r. simpl. rewrite plus_0_r.  
+      rewrite NPeano.Nat.add_sub_assoc. rewrite minus_plus. trivial.
+      SearchAbout le mult. specialize (mult_le_compat_r 1 k n). simpl; rewrite plus_0_r. simpl; intros. apply H0. omega.
+  rewrite H0 in H; clear H0. 
+  apply (list_splitLength _ _ _ H).
+Qed.   
 
-Lemma len_min : forall {A : Type} (l : list A), length l >= 0%nat.
+(*NEW, should be put into pure_lemmas or so *) Lemma skipn_short {A}: forall n (l:list A), (length l <= n)%nat -> skipn n l = nil.
+Proof. induction n; simpl; intros. 
+  destruct l; trivial. simpl in H. omega.
+  destruct l; trivial.
+  apply IHn. simpl in H. omega.
+Qed.
+
+(*NEW*) Lemma concat_length {A}: forall L (l:list A), In l L -> (length (concat L) >= length l)%nat.
+Proof.  unfold concat. induction L; simpl; intros. contradiction.
+  rewrite app_length. 
+  destruct H; subst. unfold id. omega.
+  specialize (IHL _ H). omega.
+Qed.
+
+(*NEW*) Lemma toBlocks_app_split l1 l2: length l1 = 512%nat ->
+      toBlocks (l1 ++ l2) = toBlocks l1 ++ toBlocks l2.
+Proof. intros.
+  rewrite toBlocks_equation. rewrite app_length. 
+  rewrite firstn_exact; trivial.
+  rewrite skipn_exact; trivial.
+  remember (l1 ++ l2).
+  destruct l. 
+  { assert (@length bool nil = length (l1 ++ l2)). rewrite <- Heql; trivial.
+    rewrite app_length, H in H0. rewrite plus_comm in H0. simpl in H0. omega. }
+  { rewrite  leb_correct_conv. 2: rewrite H, plus_comm; omega.
+    remember (toBlocks l2).
+    rewrite toBlocks_equation.
+    destruct l1. simpl in H; omega. 
+    rewrite leb_correct_conv. 2: rewrite H; omega.
+    rewrite firstn_same. 2: rewrite H; omega. 
+    rewrite skipn_short. 2: rewrite H; omega. 
+    rewrite toBlocks_equation. trivial. }
+Qed. 
+
+(*NEW*) Lemma sha_splitandpad_inc_InBlocks m : InBlocks 512 (sha_splitandpad_inc m).
+Proof. intros. apply InBlocks_len.
+  destruct (sha_splitandpad_inc_length m) as [k [K HK]].
+  rewrite HK. exists k. trivial.
+Qed.
+
+(*NEW*) Lemma InBlocks_Forall_512 b: (InBlocks 512 b) ->
+      Forall (fun x : list bool => length x = 512%nat) (toBlocks b).
+Proof. intros.
+  remember (toBlocks b). generalize dependent b.
+  induction l. simpl; intros. constructor.
+  simpl; intros. rewrite toBlocks_equation in Heql. destruct b. discriminate.
+  inv H.
+  rewrite H1, app_length, H0 in Heql. 
+  rewrite leb_correct_conv in Heql. 2: omega.
+  rewrite firstn_exact in Heql; trivial.
+  rewrite skipn_exact in Heql; trivial. inv Heql.
+  constructor. trivial.
+  apply (IHl _ H2 (eq_refl _)).
+Qed.
+
+(*NEW*) Lemma sha_splitandpad_blocks_512 m:
+      Forall (fun x => length x = 512%nat) (sha_splitandpad_blocks m).
+Proof. apply InBlocks_Forall_512. apply sha_splitandpad_inc_InBlocks. 
+Qed.
+
+Lemma len_min : forall {A : Type} (l : list A), (length l >= 0)%nat.
 Proof. intros. destruct l; simpl. omega. omega. Qed.
 
 Theorem fold_hash_blocks_eq_ind : forall (l : list Blist) (iv : Blist),
@@ -236,7 +393,7 @@ Lemma fpad_list_concat_eq :
   HMAC_List.app_fpad = HMAC_Concat.app_fpad.
 Proof. reflexivity. Qed.
 
-Lemma mult_triv x : forall y, y=2%nat -> x * y = x*2.
+Lemma mult_triv x : forall y, y=2%nat -> (x * y = x*2)%nat.
 Proof. intros. subst. omega. Qed.
 
 Lemma fold_left_iv_length: forall k (HK: forall iv x, length iv = k -> length (sha_h iv x) = k) l iv x , 
@@ -299,7 +456,7 @@ Proof.
   * 
   (*  Forall (fun x : list bool => length x = 512%nat)
      (sha_splitandpad_blocks m) *)
-    admit.
+    apply sha_splitandpad_blocks_512.
   * apply BLxor_length. apply k_len. apply op_len.
   * apply BLxor_length. apply k_len. apply ip_len.
   * apply BLxor_length. apply k_len. apply op_len.
