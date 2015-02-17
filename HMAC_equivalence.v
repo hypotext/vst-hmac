1eeb0e9e19c64f8859f5734c795a7c3c28f5bb8b
Lennart Beringer <eberinge@princeton.edu>
2015-02-16 19:48:42 -0500

    Success: proved the last (and most tricky) hypohtesis of HMAC_spec_abstract: length_splitandpad_inner. Probably, some of the auiliary definitions/lemmas can be cleaned up a bit, or be eliminated

--- HMAC_equivalence.v
+++ HMAC_equivalence.v
@@ -9,6 +9,7 @@ Require Import ByteBitRelations.
 Require Import HMAC_common_defs.
 Require Import HMAC_common_lemmas.
 Require Import HMAC_spec_abstract.
+Require Import common_lemmas.
 
 Require Import BinNums.
 
@@ -91,13 +92,126 @@ Proof. intros. unfold fpad_v, of_list_length.
   subst; reflexivity.
 Qed.
 
-Definition splitAndPad_v (m: Blist): list (Bvector b). Admitted.
+Require Import HMAC_spec_list.
+Definition Forall_tl (A : Type) (P : A -> Prop) (a : A) (l : list A) (H : Forall P (a :: l)):
+      Forall P l.
+inversion H. assumption.
+Defined. (*This is an alternative to FORALL that works better in the proofs below*)
 
-Lemma length_splitandpad_inner : forall (m : Blist),
+Fixpoint splitAndPad_aux (m:list Blist) (M: Forall (fun x => length x = 512%nat) m): 
+           list (Bvector b) :=
+(match m as l return (Forall (fun x => length x = 512%nat) l) -> list (Bvector b) with
+  nil => fun F => nil
+|  cons h t => fun F => cons (of_list_length _ _ h (Forall_inv F)) 
+                    (splitAndPad_aux t (Forall_tl _ _ _ _ F))
+ end) M.
+
+Lemma splitAndPad_aux_consD: forall h t M, splitAndPad_aux (cons h t) M =
+        cons (of_list_length _ _ h (Forall_inv M)) 
+                    (splitAndPad_aux t (Forall_tl _ _ _ _ M)).
+Proof. reflexivity. Qed.   
+Lemma splitAndPad_aux_cons_elim: forall h t M, exists pf1, exists pf2,
+      splitAndPad_aux (cons h t) M =
+        cons (of_list_length _ _ h pf1) (splitAndPad_aux t pf2).
+Proof. intros. exists (Forall_inv M). exists (Forall_tl _ _ _ _ M). reflexivity. Qed.
+
+Lemma splitAndPad_aux_cons_elim': forall h t M, 
+      splitAndPad_aux (cons h t) M =
+        cons (of_list_length _ _ h (Forall_inv M)) (splitAndPad_aux t (Forall_tl _ _ _ _ M)).
+Proof. reflexivity. Qed.
+
+
+Lemma  splitAndPad_aux_cons_intro: forall h t pf1 pf2, exists M,
+      splitAndPad_aux (cons h t) M =
+        cons (of_list_length _ _ h pf1) (splitAndPad_aux t pf2).
+Proof. intros. 
+  exists (Forall_cons _ pf1 pf2). rewrite splitAndPad_aux_cons_elim'. simpl.
+  reflexivity. (*This proof does not work for FORALL instead of forall_tl*)
+Qed.
+
+(*Definition splitAndPad_aux (m:list Blist) (M: Forall (fun x => length x = 512%nat) m): 
+           list (Bvector b).
+  induction m. apply nil.
+  apply (cons (of_list_length _ _ _ (Forall_inv M)) (IHm (FORALL _ _ _ _ M))).
+Defined.
+*)
+
+Lemma of_length_proof_irrel n a: forall M, 
+      Vector.to_list (of_list_length bool n a M) = a.
+Proof. intros.
+  unfold of_list_length. destruct M. apply Vector.to_list_of_list_opp. 
+Qed.
+
+Lemma splitAndPad_aux_nil m M: m=nil -> splitAndPad_aux m M= nil.
+Proof. intros; subst. reflexivity. Qed.
+
+Definition splitAndPad_v (m: Blist): list (Bvector b).
+  apply (splitAndPad_aux (toBlocks (sha_splitandpad_inc m))).
+  apply InBlocks_Forall_512. apply sha_splitandpad_inc_InBlocks.
+Defined.
+
+Lemma splitAndPad_nil: (*exists blk, *) exists pf,
+  splitAndPad_v nil = 
+  cons (of_list_length bool 512 (sha_splitandpad_inc nil) pf) nil. 
+(*cons blk nil /\
+   exists pf, exists xx, blk = of_list_length bool 512 xx pf*)
+Proof. unfold splitAndPad_v. 
+(*  unfold splitAndPad_aux. simpl.*)
+  remember (InBlocks_Forall_512 (sha_splitandpad_inc_InBlocks nil)). clear Heqf.
+  remember (toBlocks (sha_splitandpad_inc nil)).
+  rewrite toBlocks_equation in Heql.
+  remember (sha_splitandpad_inc nil) as m.
+  destruct m. exfalso. clear Heql f l.
+    unfold sha_splitandpad_inc in Heqm. 
+    assert (@length bool nil = length (bytesToBits (pad_inc (bitsToBytes nil)))).
+      rewrite <- Heqm; trivial.
+    simpl in H; clear Heqm. omega.
+  rewrite leb_correct_conv in Heql.
+    Focus 2. rewrite Heqm; simpl; omega.
+  rewrite firstn_same in Heql.
+    Focus 2. rewrite Heqm; simpl; omega.
+  rewrite pure_lemmas.skipn_short in Heql.
+    Focus 2. rewrite Heqm; simpl; omega.
+  rewrite toBlocks_equation in Heql. subst l.
+  simpl. eexists. reflexivity.
+Qed.
+
+Lemma  splitAndPad_aux_cons_intro': forall l h t (L: l = cons h t) pf1 pf2, exists M,
+      splitAndPad_aux l M =
+        cons (of_list_length _ _ h pf1) (splitAndPad_aux t pf2).
+Proof. intros. subst l. apply splitAndPad_aux_cons_intro. Qed.
+
+Lemma length_splitandpad_inner_aux: forall ssm pf,
+  Forall2
+  (fun (bv : Vector.t bool b) (bl : list bool) => bl = Vector.to_list bv)
+  (splitAndPad_aux (toBlocks ssm) pf)
+  (toBlocks ssm).
+Proof. 
+  intros ssm. remember (toBlocks ssm). generalize dependent ssm.
+  induction l.
+  { simpl; intros. constructor. }
+  { intros. rewrite toBlocks_equation in Heql.
+    destruct ssm. discriminate.
+    remember ( Compare_dec.leb (length (b :: ssm)) 511) as d.
+    destruct d. exfalso. 
+           rewrite Heql in pf. apply Forall_inv in pf.  clear Heql. 
+           rewrite firstn_length in pf.
+           symmetry in Heqd. apply leb_complete in Heqd. 
+           eapply NPeano.Nat.min_l_iff in pf. omega.
+    rewrite splitAndPad_aux_cons_elim'.
+    constructor.
+      rewrite of_length_proof_irrel. trivial.
+    eapply (IHl (skipn 512 (b :: ssm))).
+      remember (firstn 512 (b :: ssm)) as zz.
+      remember (toBlocks (skipn 512 (b :: ssm))) as yy.
+      clear - Heql. inversion Heql. trivial. }
+Qed.
+    
+Lemma length_splitandpad_inner m :
    Forall2
      (fun (bv : Vector.t bool b) (bl : list bool) => bl = Vector.to_list bv)
-     (splitAndPad_v m) (HMAC_spec_list.sha_splitandpad_blocks m).
-Admitted.
+     (splitAndPad_v m) (sha_splitandpad_blocks m).
+Proof. apply length_splitandpad_inner_aux. Qed.
 
 Lemma Equivalence kv m 
       (LM: NPeano.divide 8 (length m))
